99624c622a0f91b8adef33cf5d2beeb7e5410b94
Emonadeo <emonadeo@gmail.com>
2022-04-02 18:46:53 +0200

    Added sample REST endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,45 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const app = Fastify({
-	logger: true,
+const app = Fastify();
+
+app.get('/users', async (_, res) => {
+	const users = await prisma.user.findMany({
+		include: {
+			levels: true,
+		},
+	});
+	res.send(users);
 });
 
-app.get('/users', (req, res) => {});
-app.get('/levels', (req, res) => {});
+app.get('/levels', async (_, res) => {
+	const levels = await prisma.level.findMany();
+	res.send(levels);
+});
+
+app.get<{
+	Params: LevelByIdParam;
+}>('/levels/:id', async (req, res) => {
+	const { id } = req.params;
+	const level = await prisma.level.findUnique({
+		where: { id: Number(id) },
+		include: {
+			creators: true,
+			user: true,
+			verifier: true,
+		},
+	});
+	res.send(level);
+});
 
 // Run the server
 app.listen(3000, (err, address) => {
 	if (err) {
-		app.log.error(err);
+		console.error(err);
 		process.exit(1);
 	}
 });
+
+interface LevelByIdParam {
+	id: string;
+}
