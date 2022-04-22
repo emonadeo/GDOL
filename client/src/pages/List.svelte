653b5db26b78da2485684d58855a3e979e0c4dc2
Emonadeo <emonadeo@gmail.com>
2022-04-22 02:37:49 +0200

    Added list sections (hardcoded atm)

--- client/src/pages/List.svelte
+++ client/src/pages/List.svelte
@@ -20,7 +20,19 @@
 </script>
 
 <div class="page-list">
-	<aside>X</aside>
+	<aside>
+		<ul class="sections" role="list">
+			<li>
+				<a href="" class="type-label-lg">Main</a>
+			</li>
+			<li>
+				<a href="" class="type-label-lg">Extended</a>
+			</li>
+		</ul>
+		<div class="archive">
+			<a href="" class="archive type-label-lg">Archive</a>
+		</div>
+	</aside>
 	<main>
 		<ol role="list" class="levels">
 			{#each levels as level, i}
@@ -56,10 +68,15 @@
 
 	@include screen.xl {
 		.page-list {
+			flex: 1;
+			overflow: auto;
 			display: grid;
 			grid-template-columns: 4rem 1fr 4rem;
+			grid-template-rows: minmax(0, 1fr) max-content;
 
 			.levels {
+				grid-row: 1 / 3;
+
 				.level {
 					display: grid;
 					grid-template-columns: max-content 12rem 1fr;
@@ -95,6 +112,45 @@
 						flex-direction: column;
 						gap: 1.5rem;
 					}
+				}
+			}
+
+			aside {
+				position: sticky;
+				top: 0;
+				writing-mode: vertical-rl;
+				display: flex;
+				padding-inline: 2rem;
+
+				.sections {
+					display: flex;
+					gap: 1rem;
+					align-items: center;
+
+					> li {
+						display: flex;
+						align-items: center;
+						gap: 1rem;
+
+						a {
+							color: inherit;
+						}
+					}
+
+					> li::after {
+						content: '';
+						height: 6rem;
+						display: block;
+						border-left: 1px solid white;
+					}
+				}
+
+				.archive {
+					flex: 1;
+					display: flex;
+					justify-content: flex-end;
+					align-items: center;
+					color: inherit;
 				}
 			}
 		}
