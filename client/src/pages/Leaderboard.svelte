f6cbd683e19c0692aed8ddc4e2f15e725029555e
Emonadeo <emonadeo@gmail.com>
2022-07-30 22:51:54 +0200

    Added medal illustration to Leaderboard

--- client/src/pages/Leaderboard.svelte
+++ client/src/pages/Leaderboard.svelte
@@ -15,47 +15,57 @@
 </script>
 
 <div class="page-leaderboard">
-	<table>
-		<tr class="thr">
-			<th class="rank">
-				<p class="type-title-md">Rank</p>
-			</th>
-			<th class="score">
-				<p class="type-title-md">Score</p>
-			</th>
-			<th class="player">
-				<p class="type-title-md">Player</p>
-			</th>
-			<th class="stats" />
-		</tr>
-		{#each users as user, i}
-			{@const rank = i + 1}
-			<tr>
-				<td class="rank" class:gold={i === 0} class:silver={i === 1} class:bronze={i === 2}>
-					<p class="mono">
-						<span>{rank}</span><span>{ordinal(rank)}</span>
-					</p>
-				</td>
-				<td class="score">
-					<p class="mono">
-						{user.score}
-					</p>
-				</td>
-				<td class="player">
-					<a href={`/users/${user.id}`}>
-						<p>
-							{user.name}
-						</p>
-					</a>
-				</td>
-				<td class="stats">
-					<p>
-						{user.records.length} Records
-					</p>
-				</td>
+	<aside>
+		<img class="medal" src="/src/assets/medal.svg" alt="" />
+	</aside>
+	<main>
+		<table>
+			<tr class="thr">
+				<th class="rank">
+					<p class="type-title-md">Rank</p>
+				</th>
+				<th class="score">
+					<p class="type-title-md">Score</p>
+				</th>
+				<th class="player">
+					<p class="type-title-md">Player</p>
+				</th>
+				<th class="stats" />
 			</tr>
-		{/each}
-	</table>
+			{#each users as user, i}
+				{@const rank = i + 1}
+				<tr>
+					<td
+						class="rank"
+						class:gold={i === 0}
+						class:silver={i === 1}
+						class:bronze={i === 2}
+					>
+						<p class="mono">
+							<span>{rank}</span><span>{ordinal(rank)}</span>
+						</p>
+					</td>
+					<td class="score">
+						<p class="mono">
+							{user.score}
+						</p>
+					</td>
+					<td class="player">
+						<a href={`/users/${user.id}`}>
+							<p>
+								{user.name}
+							</p>
+						</a>
+					</td>
+					<td class="stats">
+						<p>
+							{user.records.length} Records
+						</p>
+					</td>
+				</tr>
+			{/each}
+		</table>
+	</main>
 </div>
 
 <style lang="scss">
@@ -71,81 +81,112 @@
 			display: grid;
 			grid-template-columns: calc(8rem + 4px) 1fr 8rem;
 
-			table {
-				padding-top: 6rem;
+			aside {
+				position: sticky;
+				top: calc(4rem - 1px);
+				margin-top: calc(10rem - 1px);
+				padding-right: 0.75rem;
+				align-self: start;
+			}
+
+			main {
 				grid-column: 2;
 
-				tr {
-					th,
-					td {
-						text-align: start;
-						vertical-align: bottom;
-						padding: 0.75rem 1rem;
-						white-space: nowrap;
-					}
+				table {
+					padding-block: 6rem;
 
-					th {
-						position: sticky;
-						top: 0;
-						background-color: color.$background;
-						height: 4rem;
-						padding-block: 1.25rem;
-						border-bottom: 1px solid rgba(color.$on-background, 0.2);
-					}
-
-					td {
-						border-style: none none solid solid;
-						border-width: 1px;
+					tr {
 						border-color: rgba(color.$on-background, 0.2);
 
-						&:last-child {
-							border-right-style: solid;
-						}
-					}
+						&:hover {
+							border-color: white;
 
-					th.rank,
-					td.rank {
-						text-align: right;
-					}
-
-					td.rank {
-						p {
-							font-weight: 700;
+							& + tr {
+								border-top-color: white;
+							}
 						}
 
-						&.gold p {
-							@include util.text-gradient(#ffd700, #e89e00, #ffd700);
+						th,
+						td {
+							text-align: start;
+							vertical-align: bottom;
+							padding: 0.75rem 1rem;
+							white-space: nowrap;
 						}
 
-						&.silver p {
-							@include util.text-gradient(#ffffff, #c0c0c0, #c0c0c0);
+						th {
+							position: sticky;
+							top: 0;
+							background-color: color.$background;
+							height: 4rem;
+							padding-block: 1.25rem;
+							border-bottom: 1px solid rgba(color.$on-background, 0.2);
 						}
 
-						&.bronze p {
-							@include util.text-gradient(#ff8e31, #d45d00, #e8721d);
+						td {
+							border-style: solid none none solid;
+							border-width: 1px;
+							border-color: rgba(color.$on-background, 0.2);
+							border-top-color: inherit;
+
+							&:first-child {
+								border-left-color: inherit;
+							}
+
+							&:last-child {
+								border-right-style: solid;
+								border-right-color: inherit;
+							}
 						}
-					}
 
-					th.score,
-					td.score {
-						text-align: right;
-						width: 20%;
-					}
-
-					th.player,
-					td.player {
-						width: 40%;
-
-						a {
-							color: inherit;
+						&:nth-child(2) td {
+							border-top-style: none;
 						}
-					}
 
-					th.stats,
-					td.stats {
-						width: 40%;
-						border-left: none;
-						color: rgba(color.$on-background, 0.2);
+						th.rank,
+						td.rank {
+							text-align: right;
+						}
+
+						td.rank {
+							p {
+								font-weight: 700;
+							}
+
+							&.gold p {
+								@include util.text-gradient(#ffd700, #e89e00, #ffd700);
+							}
+
+							&.silver p {
+								@include util.text-gradient(#ffffff, #c0c0c0, #c0c0c0);
+							}
+
+							&.bronze p {
+								@include util.text-gradient(#ff8e31, #d45d00, #e8721d);
+							}
+						}
+
+						th.score,
+						td.score {
+							text-align: right;
+							width: 20%;
+						}
+
+						th.player,
+						td.player {
+							width: 40%;
+
+							a {
+								color: inherit;
+							}
+						}
+
+						th.stats,
+						td.stats {
+							width: 40%;
+							border-left: none;
+							color: rgba(color.$on-background, 0.2);
+						}
 					}
 				}
 			}
