99f16efe89246ff2663927c00871b33a5b3f4732
Emonadeo <emonadeo@gmail.com>
2022-08-15 05:26:30 +0200

    Added Get User By ID Endpoint

--- server/src/users/model.ts
+++ server/src/users/model.ts
@@ -12,52 +12,6 @@ export function getPoints(rank: number, requirement: number, percentage: number)
 	return 100 - rank + 1;
 }
 
-// TODO: Use redis over global variable
-export const scores: { [userId: number]: number } = {};
-
-export async function getUsers(): Promise<UserWithRecords[]> {
-	const users = await prisma.user.findMany();
-	const usersWithRecords: UserWithRecords[] = await Promise.all(
-		users.map(async (user) => {
-			const records = await prisma.record.findMany({
-				where: {
-					userId: user.id,
-				},
-				include: {
-					level: {
-						include: {
-							user: true,
-							creators: true,
-							verifier: true,
-						},
-					},
-					user: true,
-				},
-			});
-			if (scores[user.id] === undefined) {
-				let score = 0;
-				for (const record of records) {
-					const rank = await getRank(record.levelId);
-					if (rank) score += getPoints(rank, record.level.requirement, record.percentage);
-				}
-				scores[user.id] = score;
-			}
-			return {
-				id: user.id,
-				name: user.name,
-				records: records.map((record) => ({
-					...record,
-					timestamp: record.timestamp.toISOString(),
-					video: record.video || undefined,
-				})),
-				score: scores[user.id],
-			};
-		})
-	);
-
-	return usersWithRecords.sort((a, b) => b.score - a.score);
-}
-
 async function getRank(levelId: number): Promise<number | undefined> {
 	const listLogLevel = await prisma.listLogLevel.findFirst({
 		where: {
@@ -68,4 +22,102 @@ async function getRank(levelId: number): Promise<number | undefined> {
 		},
 	});
 	return (listLogLevel && listLogLevel.index + 1) || undefined;
+}
+
+async function getScore(userId: number): Promise<number> {
+	if (scores[userId] !== undefined) {
+		return scores[userId];
+	}
+
+	// TODO: Remove duplicate, e.g. use DataLoader
+	const records = await getRecords(userId);
+
+	let score = 0;
+
+	for (const record of records) {
+		const rank = await getRank(record.level.id);
+		if (rank) score += getPoints(rank, record.level.requirement, record.percentage);
+	}
+
+	scores[userId] = score;
+
+	return score;
+}
+
+async function getRecords(userId: number) {
+	const records = await prisma.record.findMany({
+		where: {
+			userId,
+		},
+		include: {
+			level: {
+				include: {
+					user: true,
+					creators: true,
+					verifier: true,
+				},
+			},
+			user: true,
+		},
+	});
+	return records.map((record) => ({
+		...record,
+		timestamp: record.timestamp.toISOString(),
+		video: record.video || undefined,
+	}));
+}
+
+// TODO: Use redis over global variable
+export const scores: { [userId: number]: number } = {};
+
+export async function getUsers(): Promise<UserWithRecords[]> {
+	const users = await prisma.user.findMany();
+	const usersWithRecords: UserWithRecords[] = await Promise.all(
+		users.map(async (user) => {
+			// TODO: Remove duplicate
+			return {
+				id: user.id,
+				name: user.name,
+				// TODO: Remove duplicate, e.g. use DataLoader
+				records: await getRecords(user.id),
+				score: await getScore(user.id),
+			};
+		})
+	);
+
+	return usersWithRecords.sort((a, b) => b.score - a.score);
+}
+
+export async function getUserById(userId: number): Promise<UserWithRecords | undefined> {
+	const user = await prisma.user.findUnique({
+		where: {
+			id: userId,
+		},
+		include: {
+			records: {
+				include: {
+					level: {
+						include: {
+							user: true,
+							creators: true,
+							verifier: true,
+						},
+					},
+					user: true,
+				},
+			},
+			levels: true,
+			levelsCreated: true,
+			levelsVerified: true,
+		},
+	});
+	// TODO: Remove duplicate
+	const userWithRecords = user && {
+		id: user.id,
+		name: user.name,
+		// TODO: Remove duplicate, e.g. use DataLoader
+		records: await getRecords(user.id),
+		score: await getScore(user.id),
+	};
+	return userWithRecords || undefined;
 }
--- server/src/users/route.ts
+++ server/src/users/route.ts
@@ -1,6 +1,6 @@
 import { FastifyPluginAsync } from 'fastify';
 import { Users } from '../generated/openapi';
-import { getUsers } from './model';
+import { getUserById, getUsers } from './model';
 
 const plugin: FastifyPluginAsync = async function (fastify) {
 	// Get Users
@@ -12,6 +12,17 @@ const plugin: FastifyPluginAsync = async function (fastify) {
 		Reply: Users.GetUsers.ResponseBody;
 	}>('/users', async function (_, res) {
 		res.send(await getUsers());
+	});
+
+	// Get User By ID
+	fastify.get<{
+		Body: Users.GetUser.RequestBody;
+		Querystring: Users.GetUser.RequestQuery;
+		Params: Users.GetUser.RequestParams;
+		Headers: Users.GetUser.RequestHeaders;
+		Reply: Users.GetUser.ResponseBody;
+	}>('/users/:id', async function (req, res) {
+		res.send(await getUserById(Number(req.params.id)));
 	});
 };
 
